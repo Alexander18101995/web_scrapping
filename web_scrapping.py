308624911764bc9754376a0811e4fb3b52840acd
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-08-21 13:57:11 +0300

    Update web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -3,7 +3,7 @@ import bs4
 from headers import headers
 response = requests.get('https://habr.com/ru/all/',headers=headers)
 text = response.text
-HUBS = {'Научно-популярное'}
+HUBS = {'3D-принтеры','Процессоры','Физика'}
 soup = bs4.BeautifulSoup(text, features='html.parser')
 articles = soup.find_all(class_='tm-article-snippet')
 for articl in articles:
@@ -14,5 +14,5 @@ for articl in articles:
     art = articl.find('h2').find('a')
     href = art.attrs['href']
     url = 'https://habr.com' + href 
-    print(art_time.text,art.text, '--->', url)
+    print(f"{art_time.text}\n{art.text}\n{url}")
     print('-----------')
